b14a719887c88b2aabf4b2b03f3065133d773e32
Viktor Bobrovsky <v_bobrovsky@mail.ru>
2017-05-09 00:33:29 +0300

    email validation added

--- public/scripts/contact.js
+++ public/scripts/contact.js
@@ -4,17 +4,27 @@ $(document).ready(function(){
 		var $userEmail = $('input[name=email]').val();
 		var $userMessage = $('textarea[name=message]').val();
 
-		$.ajax({
-			url: '/contact',
-			method: 'POST',
-			data: {
-				"name": $userName,
-				"email": $userEmail,
-				"message": $userMessage
-			}
-		}).then(function(){
-			console.log(res);
-		});
+		var regExp = /@/g;
+		var dogFind = regExp.test($userEmail);
+
+		if ($userName && dogFind  && $userMessage) {
+			$.ajax({
+				url: '/contact',
+				method: 'POST',
+				data: {
+					"name": $userName,
+					"email": $userEmail,
+					"message": $userMessage
+				}
+			}).then(function(){
+				console.log(res);
+			});
+		} else if(dogFind == false && $userName && $userMessage ){
+			alert("Please enter correct Email!");
+			$('input[name=email]').removeAttr('placeholder');
+		} else if($userName == "" || $userName == null || $userEmail == "" || $userEmail == null || $userMessage=="" || $userMessage== null){
+			alert("Please fill all filds")
+		};
 
 	});
 
